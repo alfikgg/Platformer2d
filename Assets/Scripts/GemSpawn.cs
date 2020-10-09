38ee14d2a5b228fc24ae9200ee4f281bb2bc8728
alfikgg <alfikgg@gmail.com>
2020-10-09 23:22:31 +0300

    Add gem script

---
+++ Assets/Scripts/GemSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawn : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private GameObject _gemPrefab;
+
+    private List<Transform> _spawnPoints = new List<Transform>();
+    void Start()
+    {
+        for (int i = 0; i < _spawnPoint.childCount; i++)
+        {
+            _spawnPoints.Add(_spawnPoint.GetChild(i));
+            Debug.Log(_spawnPoints[i].transform.name);
+        }
+
+        for (int i = 0; i < _spawnPoints.Count; i++)
+        {
+            Instantiate(_gemPrefab, _spawnPoints[i].position, Quaternion.identity);
+        }
+    }
+}
