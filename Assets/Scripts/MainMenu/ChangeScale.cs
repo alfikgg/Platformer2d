ba4f789abdb8f77d321015c20e6f5aa8fbc1a13e
alfikgg <alfikgg@gmail.com>
2020-12-09 11:44:06 +0200

    modify button

---
+++ Assets/Scripts/MainMenu/ChangeScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeScale : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Vector3 _startScale;
+    [SerializeField] private Vector3 _targetScale;
+
+    private void Start()
+    {
+        _startScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = _targetScale;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = _startScale;
+    }
+}
